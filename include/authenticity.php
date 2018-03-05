4fffc72639c706a794a3e531dd87ff005ac68e76
Shehan Tis <shehanproductions@ymail.com>
2018-03-05 06:51:55 +0530

    WIP

--- include/authenticity.php
+++ include/authenticity.php
@@ -2,12 +2,12 @@
 
 // if logged in an navigated to login page redirect him to the main page
 $actual_link = "$_SERVER[REQUEST_URI]";
+var_dump($actual_link);
 if(isset($_COOKIE['Authorization'])){
 
     if($actual_link == "/ostrio/car-wash/auth/"){
         header("Location: ../");
     }
-    var_dump($actual_link);
 }else{
     if($actual_link == "/ostrio/car-wash/"){
         header("Location: auth/");
